9835091505d09532aefead791f004a8c7f69a373
MargulisMax <margulismax@gmail.com>
2018-08-27 22:34:38 +0300

    Answers to OPSschool's home assignment ver.1

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+sudo apt-get install sshpass
+ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa -q
+sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server1
