8c276a9d9c91c1c19f9cf01a3907a717f168f5fa
MargulisMax <margulismax@gmail.com>
2018-08-30 01:01:13 +0300

    Final Commit HomeAssignment Fixed Password Issue v5

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
 sudo apt-get install sshpass
-<<<<<<< HEAD
-ssh-keyscan server1 >> .ssh/known_hosts
-ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa -q
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server1
-=======
 ssh-keygen -t rsa -b 2048 -N "" -f .ssh/id_rsa -q
 #ssh-keyscan -H 192.168.100.10 >> .ssh/known_hosts
 chown vagrant:vagrant .ssh/*
@@ -15,8 +10,7 @@ echo '#!/bin/bash' > /tmp/sshostup.sh
 echo "exec &> /tmp/capture.log" >> /tmp/sshostup.sh
 echo "while [ \`ping -c 1 server1; echo \$?\` != 0 ]; do sleep 10; done" >> /tmp/sshostup.sh
 echo "su vagrant -c 'ssh-keyscan -H server1 >> /home/vagrant/.ssh/known_hosts'" >> /tmp/sshostup.sh
-echo "sshpass -p vagrant ssh-copy-id -o StrictHostkeyChecking=no -i /home/vagrant/.ssh/id_rsa.pub vagrant@server1" >> /tmp/sshostup.sh
+echo "sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub vagrant@server1" >> /home/vagrant/.bashrc
 echo "chown vagrant:vagrant .ssh/known_hosts" >> /tmp/sshostup.sh 
 
 bash /tmp/sshostup.sh &
->>>>>>> 2700fbdc5bcac3bb8ff37ca44b7f717c5ae956d2
