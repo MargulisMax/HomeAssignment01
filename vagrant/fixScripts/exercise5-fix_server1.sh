3d4c4597734fc79756229877138fb0dd342d2a8a
Max Margulis <maxm@imc-grp.com>
2018-08-29 11:29:56 +0300

    Modifiying HomeAssignment v3

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 sudo apt-get install sshpass
-ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa -q
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server2
+ssh-keygen -t rsa -b 2048 -N "" -f .ssh/id_rsa -q
+ssh-keyscan -H 192.168.100.11 >> .ssh/known_hosts
+sshpass -p vagrant ssh-copy-id -i .ssh/id_rsa.pub vagrant@192.168.100.11
--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
 sudo apt-get install sshpass
-ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/id_rsa -q
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@server1
+ssh-keygen -t rsa -b 2048 -N "" -f .ssh/id_rsa -q
+ssh-keyscan -H 192.168.100.10 >> .ssh/known_hosts
+sshpass -p vagrant ssh-copy-id -i .ssh/id_rsa.pub vagrant@192.168.100.10
